bcc1a29356e0585cd3df05522d264751d3d68cd4
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2022-10-09 16:32:53 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { MainInvoiceComponent } from './main-invoice/main-invoice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'MainInvoice', component: MainInvoiceComponent },
+  { path: 'InvoiceList', component: InvoiceListComponent },
+  { path: 'Invoice', component: InvoiceComponent },
+  { path: 'Invoice/:idFacture/:active/', component: InvoiceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainInvoiceComponent } from './main-invoice/main-invoice.component';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInvoiceComponent,
+    InvoiceListComponent,
+    InvoiceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
